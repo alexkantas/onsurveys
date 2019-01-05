533b99c2cc3989cae6888b514787512a714c24e4
Olympia <olynk@hotmail.gr>
2019-01-06 01:03:20 +0200

    change custom log-in middleware with morgan js

--- index.mjs
+++ index.mjs
@@ -3,6 +3,7 @@ import path from 'path'
 import ejs from 'ejs'
 import mainRouter from './routes/mainRoutes'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 import session from 'express-session'
 import dbConnection from './utils/dbConnection'
 
@@ -13,16 +14,11 @@ const port = process.env.PORT;
 //view engine that we use
 app.use('/public', express.static('public'))
 app.use('/public', express.static('node_modules/bootstrap/dist/'))
-app.use(whereUserIs)
+app.use(morgan('tiny'))
 app.set('views', './views')
 app.set('view engine', 'ejs')
 app.engine('ejs', ejs.renderFile)
 app.use('/', mainRouter)
-
-function whereUserIs(req, res, next) {
-  console.log(`A user visits ${req.originalUrl}`);
-  next();
-}
 
 app.use((req, res, next) => {
   res.status(404).json({ success: false, error: 'Page not exist' })
