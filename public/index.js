6d0e41f0d8b2060e1da16cfdc5b674493cf9f4ca
Olympia <olynk@hotmail.gr>
2019-01-06 03:10:48 +0200

    delete unnecessary file

--- public/index.js
+++
@@ -1,6 +0,0 @@
-const body = document.querySelector('body');
-const bnyBtn = document.querySelector('#bnyButton');
-bnyBtn.addEventListener('click', () => {
-    if (body.classList.contains('blackNyellow')) return body.classList.remove('blackNyellow');
-    body.classList.add('blackNyellow')
-})
